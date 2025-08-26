73e5a251b8e83ce7c30f630551498f3d71f23af5
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-08-26 11:23:59 -0300

    Fix VITE_API_URL environment variable usage in docker-compose.yml and API.js

--- web-admin/src/API/API.js
+++ web-admin/src/API/API.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const HOST =
-  process.env.REACT_APP_API_URL ||
-  import.meta.env.VITE_API_URL ||
-  "http://localhost:3001";
+const HOST = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 const SERVICE_API = axios.create({
   baseURL: `${HOST}/api/v1`,
