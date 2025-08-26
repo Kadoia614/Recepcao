5a6eef5bc2b6eae89b2e9bbd1a5ff2a0cb47409e
Kadoia614 <miguel_moraes2001@hotmail.com>
2025-08-26 10:28:12 -0300

    Refactor API.js to enhance HOST variable definition for improved flexibility in environment configuration

--- web-admin/src/API/API.js
+++ web-admin/src/API/API.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const HOST = process.env.REACT_APP_API_URL || "http://localhost:3001";
+const HOST =
+  process.env.REACT_APP_API_URL ||
+  import.meta.env.VITE_API_URL ||
+  "http://localhost:3001";
 
 const SERVICE_API = axios.create({
   baseURL: `${HOST}/api/v1`,
